db9f8bacd28927a39ed683012c042c7366e0e16c
luoxiao123321 <1196163003@qq.com>
2017-11-08 09:24:51 +0800

    123

--- node/routes/index.js
+++ node/routes/index.js
@@ -10,7 +10,7 @@ var pool = mysql.createPool({
 
 /* GET home page. */
 router.get('/title',function(req,res,next){
-	connection.query('SELECT id,title,detail FROM model', function(err, rows, fields) {
+	pool.query('SELECT id,title,detail FROM model', function(err, rows, fields) {
     	res.header('Access-Control-Allow-Origin',"*")
         if (err) throw err;
         	res.send(rows);
@@ -19,7 +19,7 @@ router.get('/title',function(req,res,next){
 
 router.post('/delete',function(req,res,next){
 	var abc=req.body.id;
-	connection.query('DELETE FROM model WHERE id='+abc+' ',function(err, rows, fields) {
+	pool.query('DELETE FROM model WHERE id='+abc+' ',function(err, rows, fields) {
 	    res.header('Access-Control-Allow-Origin',"*")
 	    if (err) throw err;
 	    res.send(rows);
@@ -30,9 +30,9 @@ router.post('/insert',function(req,res,next){
 	res.header('Access-Control-Allow-Origin',"*")
 	var title=req.body.title;
 	var detail=req.body.detail;
-	connection.query(`INSERT INTO model (title,detail) VALUES ('${title}','${detail}')`, function(err, rows, fields) {
+	pool.query(`INSERT INTO model (title,detail) VALUES ('${title}','${detail}')`, function(err, rows, fields) {
 	    if(rows!=""||rows!=null){
-	        connection.query("SELECT * FROM model",function(err,rows){
+	        pool.query("SELECT * FROM model",function(err,rows){
 	        	res.send(rows)
 	      	})
 	    }
@@ -42,7 +42,7 @@ router.post('/insert',function(req,res,next){
 
 router.post('/select',function(req,res,next){
 	var id=req.body.id;
-	connection.query(`SELECT id,title,detail FROM model WHERE id=${id}`,function(err, rows, fields) {
+	pool.query(`SELECT id,title,detail FROM model WHERE id=${id}`,function(err, rows, fields) {
 	    res.header('Access-Control-Allow-Origin',"*")
 	    if (err) throw err;
 	    res.send(rows);
@@ -54,7 +54,7 @@ router.post('/updata',function(req,res,next){
 	var title=req.body.title;
 	var detail=req.body.detail;
 	console.log(id,title,detail)
-	connection.query(`UPDATE work SET title='${title}',detail='${detail}' WHERE id=${id}`,function(err, rows, fields) {
+	pool.query(`UPDATE work SET title='${title}',detail='${detail}' WHERE id=${id}`,function(err, rows, fields) {
 	    res.header('Access-Control-Allow-Origin',"*")
 	    if (err) throw err;
 	    console.log(1)
